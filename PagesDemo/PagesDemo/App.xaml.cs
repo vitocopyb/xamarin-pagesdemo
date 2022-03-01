e16945b5d857dcbdaf37485ee3ec44feb681722f
Victor Sepulveda <vsepulveda@gmail.com>
2022-03-01 10:48:38 -0300

    Primer commit

---
+++ PagesDemo/PagesDemo/App.xaml.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace PagesDemo
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            //MainPage = new NavigationPage(new Page1());
+            //MainPage = new FlyoutPage1();
+            MainPage = new TabbedPage1();
+            //MainPage = new CarouselDemo();
+        }
+
+        protected override void OnStart()
+        {
+        }
+
+        protected override void OnSleep()
+        {
+        }
+
+        protected override void OnResume()
+        {
+        }
+    }
+}
---
+++ PagesDemo/PagesDemo/ContentDemo.xaml.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace PagesDemo
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class ContentDemo : ContentPage
+    {
+        public ContentDemo()
+        {
+            InitializeComponent();
+        }
+    }
+}
